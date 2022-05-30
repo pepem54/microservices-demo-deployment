bca3c6430069d24b97a3358c6f061fce42bc1f73
Piotr Pernej <98348182+pepem54@users.noreply.github.com>
2022-05-30 10:16:54 +0200

    Create wait-for-pod-phases.sh

---
+++ wait-for-pod-phases.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+iterator=1
+maxIterations=6
+error=1
+
+while [ $iterator -le $maxIterations ] 
+do
+  phases=`kubectl get pods --all-namespaces -o jsonpath="{.items[*].status.phase}" | tr -s '[[:space:]]' '\n' | sort | uniq`
+  
+  readarray -t phasesArray < <(echo "$phases")
+
+  if [ ${#phasesArray[@]} -eq 1 ]; then
+    echo "One phase"
+    if [ ${phasesArray[0]}="Running" ]; then
+      echo "Phase: running"
+      error=$((0))
+      break
+    fi
+  fi
+
+  echo "Sleeping...$iterator"
+
+  iterator=$((iterator+1))
+  sleep 10
+done
+
+if [ $error -ne 0 ]; then
+  echo "Phases: ${phasesArray[@]}"
+fi
