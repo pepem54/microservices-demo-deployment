3854a1e47c99e2a04f4b1b099c80a128d29c3b01
Piotr Pernej <98348182+pepem54@users.noreply.github.com>
2022-05-30 10:15:31 +0200

    Update and rename wait-for-pods.sh to wait-for-pod-statuses.sh

--- wait-for-pod-statuses.sh
+++ wait-for-pod-statuses.sh
@@ -6,10 +6,10 @@ error=1
 
 while [ $iterator -le $maxIterations ] 
 do
-  statuses=`kubectl get pods --all-namespaces -o jsonpath="{.items[*].status.phase}" | tr -s '[[:space:]]' '\n' | sort | uniq`
+  statuses=`kubectl get pods --all-namespaces -o jsonpath="{.items[*].status.containerStatuses[0].state}" | tr -s '[[:space:]]' '\n' | jq -r 'keys[]' jsonfile.json | uniq`
   kubectl get pods
   
-  readarray -t statusesArray < <(echo "$statuses")
+  readarray -t statusesArray < <(echo "$statuses")  
 
   if [ ${#statusesArray[@]} -eq 1 ]; then
     echo "One status"
