c2b682cee79ff8841d6e6cfa5e15e938e9928c1a
Piotr Pernej <98348182+pepem54@users.noreply.github.com>
2022-05-30 15:19:22 +0200

    Update wait-for-external-ip.sh

--- k3s-deployment/wait-for-external-ip.sh
+++ k3s-deployment/wait-for-external-ip.sh
@@ -4,19 +4,22 @@ iterator=1
 maxIterations=$1
 sleepTime=$2
              
- while [ $iterator -le $((maxIterations)) ] 
- do
-   loadBnalancerStatus=`kubectl get svc sl-boutique-frontend -o json| jq -r .status.loadBalancer`
+while [ $iterator -le $((maxIterations)) ] 
+do
+ loadBnalancerStatus=`kubectl get svc sl-boutique-frontend -o json| jq -r .status.loadBalancer`
 
-   if [ ! "$loadBnalancerStatus" == "{}" ]; then
-     echo "$loadBnalancerStatus" > loadBnalancerStatus.json
-     export BOUTIQUE_FRONTEND_IP=`jq -r .ingress[].ip loadBnalancerStatus.json`
+ if [ ! "$loadBnalancerStatus" == "{}" ]; then
+   echo "$loadBnalancerStatus" > loadBnalancerStatus.json
+   export BOUTIQUE_FRONTEND_IP=`jq -r .ingress[].ip loadBnalancerStatus.json`
 
-     if [ ! -z "$BOUTIQUE_FRONTEND_IP" ]; then
-       break
-     fi               
-   fi
+   if [ ! -z "$BOUTIQUE_FRONTEND_IP" ]; then
+     break
+   fi               
+ fi
 
-   iterator=$((iterator+1))
-   sleep $((sleepTime))
- done
+ echo "Retrying... $iterator of $maxIterations"
+ iterator=$((iterator+1))
+ sleep $((sleepTime))
+done
+
+echo "IP: $BOUTIQUE_FRONTEND_IP"
