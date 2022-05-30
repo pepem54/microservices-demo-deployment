eeb1f40cc224d19e515156d479f948b4b79868d4
Piotr Pernej <98348182+pepem54@users.noreply.github.com>
2022-05-30 15:10:18 +0200

    Create wait-for-external-ip.sh

---
+++ k3s-deployment/wait-for-external-ip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+iterator=1
+maxIterations=$1
+sleepTime=$2
+             
+ while [ $iterator -le $((maxIterations)) ] 
+ do
+   loadBnalancerStatus=`kubectl get svc sl-boutique-frontend -o json| jq -r .status.loadBalancer`
+
+   if [ ! "$loadBnalancerStatus" == "{}" ]; then
+     echo "$loadBnalancerStatus" > loadBnalancerStatus.json
+     export BOUTIQUE_FRONTEND_IP=`jq -r .ingress[].ip loadBnalancerStatus.json`
+
+     if [ ! -z "$BOUTIQUE_FRONTEND_IP" ]; then
+       break
+     fi               
+   fi
+
+   iterator=$((iterator+1))
+   sleep $((sleepTime))
+ done
